ad40f3709b62f75c5dd7d78e1afa52df2c3ccbbf
Stephen Adebayo <adebayodailola2007@gmail.com>
2026-06-14 07:57:46 -0700

    SEO: Update sitemap.xml and robots.txt with correct domain (adebayostephen.com)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/api/',
     },
-    sitemap: 'https://stephen-studio.com/sitemap.xml',
+    sitemap: 'https://adebayostephen.com/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://stephen-studio.com'
+  const baseUrl = 'https://adebayostephen.com'
   
   const pages = [
     '',
